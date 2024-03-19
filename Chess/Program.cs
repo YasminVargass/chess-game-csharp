1780a2a8fbe2e2fdf100557ce547379dc7639700
YasminVargas <yasmiin.vargass@gmail.com>
2024-03-19 15:54:05 -0300

    Criando a classe Posicao

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess.Tabuleiro;
+
+Posicao p = new Posicao(3, 4);
+
+System.Console.WriteLine("Posição: " + p);
---
+++ Chess/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿namespace Chess.Tabuleiro;
+
+public class Posicao
+{
+    public int Linha { get; private set; }
+    public int Coluna { get; private set; }
+
+
+    public Posicao(int linha, int coluna)
+    {
+        Linha = linha;
+        Coluna = coluna;
+    }
+
+    public override string ToString()
+    {
+        return
+        Linha +
+        ", " +
+        Coluna;
+    }
+
+}
